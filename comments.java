b0c679cba60421684d5c606b6b03eb284e81c774
kushvanth2001 <70205088+kushvanth2001@users.noreply.github.com>
2021-05-10 11:04:37 +0530

    Add files via upload

---
+++ comments.java
@@ -0,0 +1,11 @@
+package fundamental
+public class comments {
+    public static void main(String[] args) {
+        // (//) these are called singline comments, the comments which are in a single line can be in this ways.
+        /*(/*)these are multiline comments use for multiline comments*/
+        /*documentation comments
+        *use to write some pragraphs in this manner
+        *use for main documentation of program*/
+        //in all the above cases the compiler dosent care about them
+    }
+}
