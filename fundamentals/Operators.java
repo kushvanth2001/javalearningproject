0c66ed9a2f8ea7311ec367ff4fef43b2d2fb1253
kushvanth2001 <70205088+kushvanth2001@users.noreply.github.com>
2021-05-10 20:13:43 +0530

    Update and rename OPERATORS.java to Operators.java

--- fundamentals/Operators.java
+++ fundamentals/Operators.java
@@ -1,4 +1,4 @@
-public class operators {
+public class Operators {
     public static void main(String[] args) {
     int a = 12, b = 5;//assinment,boolean operator
     System.out.println("a + b = " + (a + b));
