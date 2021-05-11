3d0ba448ad63d154d3241334915743ca8706371e
kushvanth2001 <70205088+kushvanth2001@users.noreply.github.com>
2021-05-11 10:18:41 +0530

    Create Datatypes.java

---
+++ fundamentals/src/main/java/com/expcodes/Datatypes.java
@@ -0,0 +1,30 @@
+package fundamental;
+
+public class Datatypes {
+	char c='a';	    //2 byte
+	byte b=12;		//1 byte
+	short s=568;		//2 byte
+	int	 i=6543;	//4 byte
+	long l=142576L;	//8 byte
+	float f=1.1f;	//4 byte
+	double d=1.1234d;	//8 byte
+   //boolean size can be  precisely identified
+	 boolean b1=true;
+	void show()
+	{
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(c);
+		 System.out.println(b1); 
+	}
+	public static void main(String[] args) {
+		datatypes obj=new datatypes();
+		obj.show();
+		
+	}
+
+}
