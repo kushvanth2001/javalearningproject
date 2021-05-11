08fffafe100597d7fef0ee0c7d7fa0e97c6e48fb
kushvanth2001 <70205088+kushvanth2001@users.noreply.github.com>
2021-05-11 11:10:23 +0530

    Create Ternary.java

---
+++ fundamentals/src/main/java/com/expcodes/11-05-2021/Ternary.java
@@ -0,0 +1,9 @@
+public class Ternaryy {
+   public static void main(String[] args) {
+       int Raining = 1;
+       String Whether;
+ 
+       Whether = (Raining == 1) ? "don't forget your umbrella" : "it's a sunny day";
+       System.out.println(" Today " + Whether);
+   }
+}
