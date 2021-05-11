64aa9bcbdc1ca0d57472827613a3cad76b3a2129
kushvanth2001 <70205088+kushvanth2001@users.noreply.github.com>
2021-05-11 10:56:05 +0530

    Create Billbydiscount.java

---
+++ fundamentals/src/main/java/com/expcodes/Billbydiscount.java
@@ -0,0 +1,40 @@
+//class to show discount acording to the coustmer bill using if else ladder
+public class Billbydiscount {
+
+   public static void main(String args[]){
+	
+	int bill=99;
+	
+	if(bill <100 && bill >= 1) {//if case
+	  
+	  System.out.println(" your discount is "+bill%5);
+	  
+	}
+	
+	else if(bill < 1000 && bill >= 100) {
+	  
+	  System.out.println("your discount is"+bill%10);
+	  
+	}
+	else if(bill <10000 && bill >=1000) {
+	    
+	  System.out.println("your dicount is"+bill%15);
+	  
+	}
+	else if(bill <100000 && bill >=10000) {
+	    
+	  System.out.println("your discount is"+bill%20);			
+
+	}
+	else {
+	    
+	  
+	  System.out.println("no dicount for u");			
+	
+	}
+   
+       
+   }
+
+    
+}
