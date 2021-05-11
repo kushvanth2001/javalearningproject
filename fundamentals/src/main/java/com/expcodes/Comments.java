dce35fd381f760ca08ac547053cfc8bcf785f0f4
kushvanth2001 <70205088+kushvanth2001@users.noreply.github.com>
2021-05-11 10:16:25 +0530

    Update Comments.java

--- fundamentals/src/main/java/com/expcodes/Comments.java
+++ fundamentals/src/main/java/com/expcodes/Comments.java
@@ -1,2 +1,12 @@
-
+package fundamental
+public class Comments {
+    public static void main(String[] args) {
+        // (//) these are called singline comments, the comments which are in a single line can be in this ways.
+        /*(/*)these are multiline comments use for multiline comments*/
+        /*documentation comments
+        *use to write some pragraphs in this manner
+        *use for main documentation of program*/
+        //in all the above cases the compiler dosent care about them
+    }
 }
+
